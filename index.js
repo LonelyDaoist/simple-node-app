db5328fd477c4b54ded24efa772192634e401020
Ghassen Ghabarou <ghabaroughassen@gmail.com>
2021-07-26 16:25:00 +0200

    added newline

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from NodeJS');
+  res.send('Hello World from NodeJS.\n');
 });
 
 app.get('/whoami', (req, res) => {
-  res.send(`Sent from ${os.hostname()}`);
+  res.send(`Sent from ${os.hostname()}.\n`);
 });
 
 app.listen(PORT, HOST);
